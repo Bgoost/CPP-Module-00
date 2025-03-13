64a7c86bcdc82aeecc5a5470ecdcd09facbbc7e9
Cristina <crmanzan@gmail.com>
2025-03-13 20:23:47 +0100

    ayuda correctores

--- ex01/UserInterface.cpp
+++ ex01/UserInterface.cpp
@@ -15,6 +15,14 @@ UserInterface::~UserInterface()
 
 void UserInterface::manager()
 {
+    // phonebook.add_contact("a", "a", "a", "1", "1");
+    // phonebook.add_contact("b", "b", "b", "2", "2");
+    // phonebook.add_contact("c", "c", "c", "3", "3");
+    // phonebook.add_contact("d", "d", "d", "4", "4");
+    // phonebook.add_contact("e", "e", "e", "5", "5");
+    // phonebook.add_contact("f", "f", "f", "6", "6");
+    // phonebook.add_contact("g", "g", "g", "7", "7");
+    // phonebook.add_contact("h", "h", "h", "8", "8");
     while(1)
     {
         std::string input = prompt();
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,25 +3,7 @@
 
 int main()
 {
-    // PhoneBook phonebook;
     UserInterface interface;
-
-    // phonebook.add_contact("a", "a", "a", "1", "1");
-    // phonebook.add_contact("b", "b", "b", "2", "2");
-    // phonebook.add_contact("c", "c", "c", "3", "3");
-    // phonebook.add_contact("d", "d", "d", "4", "4");
-    // phonebook.add_contact("e", "e", "e", "5", "5");
-    // phonebook.add_contact("f", "f", "f", "6", "6");
-    // phonebook.add_contact("g", "g", "g", "7", "7");
-    // phonebook.add_contact("h", "h", "h", "8", "8");
-
     interface.manager();
-
-
-    // phonebook.display_all_contacts();
-
-
-
-
-    return 0;
+    return (0);
 }
